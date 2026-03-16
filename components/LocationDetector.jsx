906212889a718672a7af4315d294a31973f7661a
Rejoan <rej.ash11@gmail.com>
2026-03-17 04:06:45 +0600

    feature/ location detecting and redirecting to a url with lang & lat

---
+++ components/LocationDetector.jsx
@@ -0,0 +1,30 @@
+'use client'
+import { usePathname, useRouter, useSearchParams } from 'next/navigation';
+import { useState, useEffect } from 'react';
+import React from 'react';
+import Image from 'next/image'
+
+export default function LocationDetector() {
+    const pathName = usePathname();
+    const router = useRouter();
+    const searchParams = useSearchParams();
+
+    const [isLoading, setIsLoading] = useState(false);
+
+    useEffect(() => {
+        setIsLoading(true);
+        const params = new URLSearchParams(searchParams);
+        navigator.geolocation.getCurrentPosition((position) => {
+            params.set('latitude', position.coords.latitude);
+            params.set('longitude', position.coords.longitude);
+            setIsLoading(false);
+            router.push(`/current?${params.toString()}`)
+        });
+    }, [pathName, searchParams]);
+
+    return (
+        <>
+            {isLoading && <div> Loading... </div>}
+        </>
+    )
+}
